a6aa3b3513710ab04d39fd998f0872728a0847b4
abishek <abishek004m@gmail.com>
2025-12-12 20:48:29 +0530

    Fix router and image paths for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductDetail from './pages/ProductDetail';
 import { useState } from 'react';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Cart from './pages/Cart';
 
@@ -14,19 +14,23 @@ function App() {
 
   return (
     <div className="App">
-        <Router>
-          <div>
-            <ToastContainer theme='dark' position='top-center' />
-            <Header cartItems={cartItems} />
-            <Routes>
-                <Route path="/"  element={ <Home />}/>
-                <Route path="/search"  element={ <Home />}/>
-                <Route path="/product/:id"  element={ <ProductDetail cartItems={cartItems}  setCartItems={setCartItems}  />}/>
-                <Route path="/cart"  element={ <Cart cartItems={cartItems}  setCartItems={setCartItems}  />}/>
-            </Routes>
-          </div>
-        </Router>
-      <Footer/>
+      <Router>
+        <ToastContainer theme="dark" position="top-center" />
+        <Header cartItems={cartItems} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Home />} />
+          <Route
+            path="/product/:id"
+            element={<ProductDetail cartItems={cartItems} setCartItems={setCartItems} />}
+          />
+          <Route
+            path="/cart"
+            element={<Cart cartItems={cartItems} setCartItems={setCartItems} />}
+          />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
